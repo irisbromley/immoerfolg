ac61b79e993f97d6b77dfa1b4ddf647199751f45
Iris Bromley <iris.j.bromley@gmail.com>
2019-05-09 10:15:36 +0200

    changed Rendering, update php file

---
+++ send-email.php
@@ -0,0 +1,15 @@
+<?php
+$anrede = $_POST['anrede'];
+$titel = $_POST['titel'];
+$name = $_POST['name'];
+$email = $_POST['email'];
+$phone = $_POST['phone'];
+$message = $_POST['message'];
+$body="_________________________________ \n Anfrage für IMMOERFOLG: \n Absender: $anrede $titel $name \n E-Mail: $email \n Telefonnummer: $phone \n Nachricht: $message ";
+$to = "iris.pfeffer@gmx.at";
+$subject = "Anfrage für IMMO";
+if($name != "" && $email != ""){  
+    echo mail($to, $subject, $body)
+} else {   
+    echo "false";
+}
