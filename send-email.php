40e796ae5365e4386accde8d507db01761bb68b3
Iris Bromley <iris.j.bromley@gmail.com>
2019-05-22 11:31:00 +0200

    fix modal, replace email in php

--- send-email.php
+++ send-email.php
@@ -6,7 +6,7 @@ $email = $_POST['email'];
 $phone = $_POST['phone'];
 $message = $_POST['message'];
 $body="_________________________________ \n Anfrage für IMMOERFOLG: \n Absender: $anrede $titel $name \n E-Mail: $email \n Telefonnummer: $phone \n Nachricht: $message ";
-$to = "office@immoerfolg.at";
+$to = "info@firstred.net";
 $subject = "Anfrage für IMMO";
 if($name != "" && $email != ""){  
     echo mail($to, $subject, $body);
